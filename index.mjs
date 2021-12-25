c452b207434db89a7f526f4f12d7767cf1efc60f
Michael Zant <miza@pop-os.localdomain>
2021-12-25 13:10:47 +0100

    first routing

--- index.mjs
+++ index.mjs
@@ -1,15 +1,43 @@
 import inquirer from 'inquirer'
 
+const yesOrNo = value => {
+  const reg = new RegExp('(y(es)?|no?)', 'i')
+  return reg.test(value) ? true : 'Please write "yes" or "no"';
+}
+
+const isNo = value => {
+  const reg = new RegExp('no?', 'i')
+  return reg.test(value);
+}
+
+const inquirerConf = {
+  start: {
+    type: 'input',
+    name: 'playing',
+    message: 'Do you want to play a game? (y/n)',
+    validate: yesOrNo
+  },
+  selectGame: {
+    type: 'list',
+    name: 'game',
+    message: 'Which Game would you like to play?',
+    choices: () => Object.keys(router),
+  },
+  guessing: {
+    type: 'input',
+    name: 'guessing',
+    message: 'Guess a number',
+  },
+}
+
 const Task = fork => ({
   fork,
   map: fn => Task((rej, res) => fork(rej, x => res(fn(x)))),
   chain: other => Task((rej, res) => fork(rej, x => other(x).fork(rej, res))),
 })
 
-const Input = (obj) => ({ type: 'input', ...obj})
-
-const getInput = obj => 
-  Task((rej, res) => inquirer.prompt(Input(obj))
+const getUserInput = obj => 
+  Task((rej, res) => inquirer.prompt(obj)
     .then(answer => res(answer[obj.name]))
     .catch((error) => 
       (error.isTtyError) ? 
@@ -18,17 +46,21 @@ const getInput = obj =>
   )
 
 const start = () => 
-  getInput({
-    name: 'playing',
-    message: 'Do you want to play a game? (y/n)'
-  })
-    .chain(answer => Task((rej, res) => {
-      (answer === 'n') ? rej('Quit game') : res(answer)
-    })
-    )
+  getUserInput(inquirerConf.start)
+    .map(answer => isNo(answer) ? quit : selectGame)
 
+const quit = () => Task((rej, _) => rej('Quit game'))
 
-const runApp = () =>
-  start().fork(console.log, runApp)
+const selectGame = () =>
+  getUserInput(inquirerConf.selectGame)
+  .map(answer => router[answer])
 
-runApp()
+const guessing = () =>
+  getUserInput(inquirerConf.guessing)
+
+const router = { guessing }
+
+const runApp = f =>
+  f().fork(console.log, runApp)
+
+runApp(start)
